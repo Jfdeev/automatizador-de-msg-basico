56f88a51c505f6e4d6d47d8bd5b7bf65a3898274
Jfdeev <jfrsoares03@gmail.com>
2024-12-17 21:22:10 -0300

    finish

---
+++ bot.py
@@ -0,0 +1,15 @@
+import pyautogui as bot
+import random
+import time
+
+time.sleep(5)
+
+
+msg = "TE AMO MUITO MEU AMOR, VC É A MELHOR COISA QUE ME ACONTECEU, EU TE AMO"
+
+for i in range(1000):
+    bot.write(msg)
+    bot.press("enter")
+    time.sleep(0.5)
+
+print("FIM")
